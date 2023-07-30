ad80ea882938f6cd89525fb8b22c3e980fbd0b72
unknown <rhyz@xtendly.com>
2023-07-30 12:21:46 +0800

    added newly created modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { UploadLibraryModule } from './upload-library/upload-library.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    UserModule,
+    UploadLibraryModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
