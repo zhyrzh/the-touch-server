59565f604baca688804f47c794b688d895869f97
unknown <rhyz@xtendly.com>
2023-11-02 20:27:30 +0800

    added new service for fetching home-page related data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { ArticleService } from './article/article.service';
+import { UserService } from './user/user.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private articleService: ArticleService,
+    private userService: UserService,
+  ) {}
+
+  async getAllHomePageData() {
+    const articleData = await this.articleService.getLimitedRecent();
+    const journalistData = await this.userService.getLimitedRecent();
+
+    const normalizedArticleData = articleData.map((data) => ({
+      headline: data.headline,
+      createdAt: data.createdAt,
+      authors: data.author.map((athr) => ({
+        name: `${athr.firstName} ${athr.lastName}`,
+        email: athr.email,
+      })),
+    }));
+
+    const normalizedJournalistData = journalistData.map((data) => ({
+      email: data.email,
+      course: data.profile.course,
+      position: data.profile.position,
+      name: `${data.profile.firstName} ${data.profile.lastName}`,
+    }));
+
+    return {
+      pendingArticles: normalizedArticleData.map((item) => ({
+        ...item,
+      })),
+      userData: normalizedJournalistData,
+    };
   }
 }
