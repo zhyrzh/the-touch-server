57849bb1c3fbf7f8adbc16d701bc41b512013253
unknown <rhyz@xtendly.com>
2023-11-02 20:28:07 +0800

    added new route for fetching home-page related data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/home-page-data')
+  getAllHomePageData() {
+    return this.appService.getAllHomePageData();
   }
 }
